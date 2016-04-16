a8ef78fb113bf000c582eb91e3b1cf231ed64ed4
meisterfuu <info@meister-fuu.de>
2016-04-16 08:36:26 +0200

    Init

---
+++ game/html/src/de/dogedev/ld35/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package de.dogedev.ld35.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import de.dogedev.ld35.LDGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new LDGame();
+        }
+}
