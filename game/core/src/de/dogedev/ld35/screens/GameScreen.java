b7f4b6b918397f2af0b7b42d33bd02aac9f20de3
meisterfuu <info@meister-fuu.de>
2016-04-16 12:12:10 +0200

    Fix

--- game/core/src/de/dogedev/ld35/screens/GameScreen.java
+++ game/core/src/de/dogedev/ld35/screens/GameScreen.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
