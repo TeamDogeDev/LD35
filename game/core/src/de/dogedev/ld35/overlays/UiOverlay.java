d89a1ff203bb63cc77b61cfa4c9bb5dce65c1fbd
meisterfuu <info@meister-fuu.de>
2016-04-18 20:21:19 +0200

    Corrected flip

--- game/core/src/de/dogedev/ld35/overlays/UiOverlay.java
+++ game/core/src/de/dogedev/ld35/overlays/UiOverlay.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.utils.Align;
 import de.dogedev.ld35.Statics;
 import de.dogedev.ld35.ashley.ComponentMappers;
+import de.dogedev.ld35.ashley.components.KeyComponent;
 import de.dogedev.ld35.ashley.components.PlayerComponent;
 import de.dogedev.ld35.assets.enums.BitmapFonts;
 import de.dogedev.ld35.assets.enums.Textures;
@@ -22,6 +23,7 @@ import java.text.DecimalFormat;
 public class UiOverlay extends AbstractOverlay {
 
     private final ImmutableArray<Entity> players;
+    private final ImmutableArray<Entity> keys;
     private BitmapFont font;
     private DecimalFormat floatFormat = new DecimalFormat("#.##");
     private PooledEngine ashley;
@@ -30,6 +32,7 @@ public class UiOverlay extends AbstractOverlay {
         init();
         this.ashley = ashley;
         players = ashley.getEntitiesFor(Family.all(PlayerComponent.class).get());
+        keys = ashley.getEntitiesFor(Family.all(KeyComponent.class).get());
     }
 
     @Override
@@ -46,6 +49,7 @@ public class UiOverlay extends AbstractOverlay {
         PlayerComponent player = ComponentMappers.player.get(players.get(0));
         batch.begin();
         font.draw(batch, ""+floatFormat.format(player.maxShiftTime-player.shiftTime), 10, Gdx.graphics.getHeight()-10, 200, Align.left, false);
+        font.draw(batch, "Keys left: "+keys.size(), 0, Gdx.graphics.getHeight()-10, 1270, Align.right, false);
         for(int i = player.maxShiftCount; i > 0; i--){
             batch.draw(Statics.asset.getTexture(Textures.CHICKWEN_SINGLE), 50+((player.maxShiftCount-i)*20), Gdx.graphics.getHeight()-25);
         }
